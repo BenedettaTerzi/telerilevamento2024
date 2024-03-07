f4a3160901e0bc5c56149e3f41913430a29929c3
BenedettaTerzi <162352424+BenedettaTerzi@users.noreply.github.com>
2024-03-07 17:50:32 +0100

    Create 02_installing_packages.R

---
+++ 02_installing_packages.R
@@ -0,0 +1,13 @@
+# Installing packages in R
+# Cran è dove ci sono tutti i pacchetti. Ci sono anche dei pacchetti su GitHub
+# Cerco su internet il pacchetto esempio pacchetto terra
+
+# Cerco su internet terra package 
+# Vedo che c’è il reference manual (un manuale con le funzioni ecc…) in oltre ci possono essere delle vignette ovvero dei piccoli manuali 
+
+# per installare un pacchetto uso la funzione install.packages()
+# Il nome del package va tra virgolette
+
+install.packages("terra")
+# per richiamarlo uso la funzione library()
+library(terra)

a9069ee243f1656d08474b2686623e45eace75a9
BenedettaTerzi <162352424+BenedettaTerzi@users.noreply.github.com>
2024-03-12 17:31:23 +0100

    Update 02_installing_packages.R

--- 02_installing_packages.R
+++ 02_installing_packages.R
@@ -12,3 +12,19 @@
 install.packages("terra")
 # per richiamarlo uso la funzione library()
 library(terra)
+
+# ora installo pacchetti da GitHub
+# Come faccio 
+# Uso la funzione install.github ma per questa funzione devo scaricare un pacchetto (devtools)
+# Poi con install.github prendo il pacchetto imagery 
+# Quindi scarico il pacchetto devtools dal CRAN, poi uso la funzione install.github e poi scarico imageRy 
+
+install.packages("devtools")
+library(devtools)  # or require 
+
+#installo il pacchetto imageRy da GitHub
+devtools::install_github("ducciorocchini/imageRy")
+# prima dei :: mi dice che è una funzione di devtools
+library(imageRy)
+
+# library() non metto le virgolette, perché sono già dentro R 

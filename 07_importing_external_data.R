d9baa3148e68df6ca87b74abc0655456ed270c6f
BenedettaTerzi <162352424+BenedettaTerzi@users.noreply.github.com>
2024-04-23 17:33:45 +0200

    Update 07_importing_external_data.R

--- 07_importing_external_data.R
+++ 07_importing_external_data.R
@@ -15,3 +15,12 @@ ocean<-rast("ocean.jpg")
 im.plotRGB(ocean,1,2,3)
 
 # copernicus ha 4 blocchi di dati (vegetazione, energia, acqua, criosfera) e altri due (hot spots e groundbased, dati presi a terra di diversa natura) e contengono tante variabili
+
+# importing data from Copernicus 
+soil<-rast("c_gls_SSM1km_202404210000_CEURO_S1CSAR_V1.2.1.nc")
+plot(soil)
+plot(soil[[1]])
+
+#crop data (uso coordinate per tagliare una immagine)
+ext <- c(25,30,55,58) # mi rappresentano x minima x massima y minima y massima in un array per definire l'estensione 
+soilcrop<-crop(soil,ext)

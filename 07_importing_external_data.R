df9a6680f8918f97ea7ff3dea55096633ad1eb89
BenedettaTerzi <162352424+BenedettaTerzi@users.noreply.github.com>
2024-04-23 16:38:37 +0200

    Update 07_importing_external_data.R

--- 07_importing_external_data.R
+++ 07_importing_external_data.R
@@ -1,1 +1,9 @@
-
+# Importing data from external sources
+# uso una funzione per spiegare qual'è la cartella in cui salviamo tutto (la directoty)
+setwd("/Users/benedettaterzi/Documents")
+eclissi<-rast("eclissi.png")
+# Warning message: unknown extent perchè non ha georeferenziazione 
+#rast funzione che ...
+# plotting the data 
+im.plotRGB(eclissi,1,2,3)
+# potrei usare anche la funzione plot del pacchetto terra 

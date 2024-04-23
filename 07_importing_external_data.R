b8c0b0fd90efba4c3cef93ab9930684bb339ff7c
BenedettaTerzi <162352424+BenedettaTerzi@users.noreply.github.com>
2024-04-23 17:04:28 +0200

    Update 07_importing_external_data.R

--- 07_importing_external_data.R
+++ 07_importing_external_data.R
@@ -1,9 +1,17 @@
 # Importing data from external sources
-# uso una funzione per spiegare qual'è la cartella in cui salviamo tutto (la directoty)
+# uso una funzione setwd ovvero set working directory per spiegare qual'è la cartella in cui salviamo tutto (la directory)
 setwd("/Users/benedettaterzi/Documents")
 eclissi<-rast("eclissi.png")
 # Warning message: unknown extent perchè non ha georeferenziazione 
-#rast funzione che ...
+#rast funzione per importare il dato
 # plotting the data 
 im.plotRGB(eclissi,1,2,3)
-# potrei usare anche la funzione plot del pacchetto terra 
+# potrei usare anche la funzione plot del pacchetto terra
+dif = eclissi[[1]]-eclissi[[2]]
+plot(dif)
+
+# import another image 
+ocean<-rast("ocean.jpg")
+im.plotRGB(ocean,1,2,3)
+
+# copernicus ha 4 blocchi di dati (vegetazione, energia, acqua, criosfera) e altri due (hot spots e groundbased, dati presi a terra di diversa natura) e contengono tante variabili
